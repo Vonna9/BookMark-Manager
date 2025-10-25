87c007b6d91a0bcacadc00704e6d0642c6d0656a
RussianGravy <valentinejones1215@gmail.com>
2025-10-25 11:32:07 -0700

    proposed functions

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,13 @@
-// pop up logic
+// pop up logic on the front end
+
+// Codes that trigger sorting:
+// - AtoZ
+// - ZtoA
+
+const trigger_sort_a_to_Z = () => {
+  chrome.runtime.sendMessage({ type: "AtoZ" });
+};
+
+const trigger_sort_z_to_a = () => {
+  chrome.runtime.sendMessage({ type: "ZtoA" });
+};
